9fd8dbd5032d2d1116abed84eb622c92780994ab
abdaziznet <dev.abdaziz@outlook.com>
2024-01-09 17:40:53 +0700

    first commit

---
+++ src/main/java/abdaziz/net/belajarjavakafka/ProducerApp.java
@@ -0,0 +1,39 @@
+package abdaziz.net.belajarjavakafka;
+
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+public class ProducerApp {
+
+    public static void main(String[] args) {
+        Properties properties = new Properties();
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        for (int i = 0; i < 1000000; i++) {
+            ProducerRecord<String, String> message = new ProducerRecord<>("helloworld",
+                    Integer.toString(i), "Hello " + i);
+
+            try {
+                producer.send(message).get();
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+
+        producer.close();
+    }
+}
---
+++ src/test/java/abdaziz/net/belajarjavakafka/BelajarJavaKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package abdaziz.net.belajarjavakafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarJavaKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
